3c439d22a28c43673a095de1f25406e5866ac4d1
Samuel Slade <samuel.slade@sladesoftware.co.uk>
2020-09-22 11:02:26 +0100

    Added standard page and layout containers to blog template

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,20 +1,38 @@
 import React from "react"
 import { graphql } from "gatsby"
 import { MDXRenderer } from "gatsby-plugin-mdx"
+import styled from "styled-components"
+import { Page, Layout } from "components"
+
+const Centered = styled.div`
+  display: flex;
+  justify-content: center;
+`
+
+const Container = styled.div`
+  max-width: 1024px;
+  margin: 0 2rem;
+`
 
 const Template = ({ data }) => {
   const { mdx } = data
   const { frontmatter, body } = mdx
 
   return (
-    <div>
-      <h1>{frontmatter.title}</h1>
-      <h2>{frontmatter.date}</h2>
-      
-      <MDXRenderer>
-        {body}
-      </MDXRenderer>
-    </div>
+    <Page title={frontmatter.title}>
+      <Layout>
+        <Centered>
+          <Container>
+            <h1>{frontmatter.title}</h1>
+            <h2>{frontmatter.date}</h2>
+
+            <MDXRenderer>
+              {body}
+            </MDXRenderer>
+          </Container>
+        </Centered>
+      </Layout>
+    </Page>
   )
 }
 
