fc93fc7c5adf265832912fc830d98a1f27935339
Samuel Slade <samuel.slade@sladesoftware.co.uk>
2020-12-10 10:20:32 +0000

    Added copy text to Services and Fees section on the about page

--- src/sections/about/ServicesAndFees.js
+++ src/sections/about/ServicesAndFees.js
@@ -4,11 +4,22 @@ import { Header, Paragraph } from "components/typography"
 const ServicesAndFees = () => (
   <div>
     <Header anchorId="services-and-fees" offset>
-      {`Services and Fees`}
+      {`Services and fees`}
     </Header>
 
     <Paragraph alignment="justify">
-      {`TODO: Talk about services`}
+      {`Our services are structured into 3 main areas and our fees are calculated
+      differently for each.`}
+    </Paragraph>
+
+    <ul>
+      <li>Ad hoc work - hourly/daily rate</li>
+      <li>Advisory work - retainer</li>
+      <li>Project work - value/delivery-based fixed fee</li>
+    </ul>
+
+    <Paragraph alignment="justify">
+      {`Take a look at our Services and Fees pages for more information.`}
     </Paragraph>
   </div>
 )
