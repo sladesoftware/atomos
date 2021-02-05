a72a0cfe29c36e769682345434df1dcea505b776
Samuel Slade <samuel.slade@sladesoftware.co.uk>
2021-02-05 09:51:21 +0000

    Added the retainer page

---
+++ src/pages/services/retainer.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Page, Layout, GutterContainer } from "components"
+
+const RetainerPage = () => (
+  <Page title="Services | Retainer">
+    <Layout>
+      <GutterContainer>
+        <div>{`TODO: Retainer Page`}</div>
+      </GutterContainer>
+    </Layout>
+  </Page>
+)
+
+export default RetainerPage
